2fad27f3ab049b5994c176f3ae160d16774038ba
Baduit <simon.bertho@epitech.eu>
2020-09-12 21:31:36 +0200

    Now there's concepts

---
+++ lib/YoloVM/MethodConcepts.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <concepts>
+
+#include <YoloVM/Types.hpp>
+
+namespace YoloVM
+{
+
+template <typename T>
+concept HasGetAt =
+	requires(T t, Integer i)
+	{
+		{ t.get_at(i) };
+	};
+
+template <typename T, typename ValueType>
+concept HasStoreAt =
+	requires(T t, Integer i, ValueType v)
+	{
+		{ t.store_at(i, std::move(v)) };
+	};
+
+template <typename T>
+concept HasSize =
+	requires(const T& t)
+	{
+		{ t.size() };
+	};
+
+template <typename T>
+concept HasResize =
+	requires(T t, Integer i)
+	{
+		{ t.resize(i) };
+	};
+
+template <typename T, typename ValueType>
+concept HasInsert =
+	requires(T t, Integer i, ValueType v)
+	{
+		{ t.insert(i, std::move(v)) };
+	};
+
+template <typename T, typename ValueType>
+concept HasPushBack =
+	requires(T t, ValueType v)
+	{
+		{ t.push_back(std::move(v)) };
+	};
+
+template <typename T>
+concept HasConcat =
+	requires(T t, const T& other)
+	{
+		{ t.concat(other) };
+	};
+
+// TODO
+
+}
