c73c263760085792abfbf73acb096804a0723c8c
Lena Bertho <lena.bertho@protonmail.com>
2023-04-19 22:03:17 +0200

    ✨: create the wasm target

--- topflight_core/src/lib.rs
+++ topflight_core/src/lib.rs
@@ -1,9 +1,9 @@
 use thiserror::Error;
-
 use topflight_definitions::{Instruction, ParseError};
-pub use topflight_definitions::{Routine, Value};
 use topflight_vm::{execute, VMError};
+
 pub use topflight_vm::{Memory, Routines};
+pub use topflight_definitions::{Routine, Value};
 
 #[derive(Error, Debug)]
 pub enum Error {
---
+++ topflight_wasm/src/lib.rs
@@ -0,0 +1,51 @@
+use wasm_bindgen::prelude::*;
+use std::fmt;
+
+#[derive(Debug)]
+struct Error {
+    wrapped_error: topflight_core::Error,
+    line_number: usize,
+    line: String,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Error at line {}: {}\n\t{}",
+            self.line_number, self.wrapped_error, self.line
+        )
+    }
+}
+
+fn execute_file_impl(code: String) -> Result<String, Error> {
+    let mut output = String::new();
+    let mut memory = topflight_core::Memory::default();
+    let mut routines = topflight_core::Routines::new();
+    let mut routine_in_construction: Option<topflight_core::Routine> = Option::None;
+    let lines = code.split('\n');
+    for (i, line) in lines.enumerate() {
+        if let Err(error) = topflight_core::handle_line(
+            line,
+            &mut memory,
+            &mut routines,
+            &mut routine_in_construction,
+            &mut output,
+        ) {
+            return Err(Error {
+                wrapped_error: error,
+                line_number: i + 1,
+                line: line.to_string(),
+            });
+        }
+    }
+    Ok(output)
+}
+
+#[wasm_bindgen]
+pub fn execute_file(code: String) -> String {
+    match execute_file_impl(code) {
+        Ok(str) => str,
+        Err(error) => format!("{}", error),
+    }
+}
